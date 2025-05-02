c59f7f5d2c58ce006c212b78b1f857b3b7b62211
Hayden Boss <hayden_boss@yahoo.com>
2025-05-02 09:52:41 -0600

    SwiftUI fav sport Pt2 completed

--- SwiftUIFavSportLayout/SwiftUIFavSportLayout/ContentView.swift
+++ SwiftUIFavSportLayout/SwiftUIFavSportLayout/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedSport: String? = nil
+    @State private var hasSubmitted = false
+    
     var body: some View {
         ZStack {
             Color.black.ignoresSafeArea(edges: .all)
@@ -27,25 +30,43 @@ struct ContentView: View {
                 }
                 Spacer()
                 Button {
+                    hasSubmitted = true
                 } label: {
                     Text("Submit")
                         .frame(width: 230, height: 25)
                 }
                 .buttonStyle(.borderedProminent)
+                
+                if hasSubmitted {
+                    if let sport = selectedSport {
+                        Text("You chose \(sport)")
+                            .foregroundStyle(.white)
+                            .padding(.top)
+                    } else {
+                    }
+                }
+                
                 Spacer()
             }
         }
     }
+    
     func fancyButton(title: String) -> some View {
         Button {
+            selectedSport = title
+            if hasSubmitted {
+                hasSubmitted = false
+            }
         } label: {
             Text(title)
                 .frame(width: 70, height: 80)
         }
         .buttonStyle(.borderedProminent)
-        .tint(Color.green)
+        .tint(.green)
+        .opacity(selectedSport == nil ? 1.0 : (selectedSport == title ? 1.0 : 0.4))
     }
 }
+
 
 
 #Preview {
